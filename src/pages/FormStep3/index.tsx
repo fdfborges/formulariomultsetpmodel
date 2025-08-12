ff65ba091bd1ddb6beda8f27bdf5a20abb3293aa
FelipeIE <felipecorpiefe@Gmail.com>
2025-08-12 17:00:48 -0300

    second commit

---
+++ src/pages/FormStep3/index.tsx
@@ -0,0 +1,10 @@
+import * as C from './styles';
+import { BrowserRouter } from 'react-router-dom';
+
+export const FormStep3 = () => {
+    return(
+        <C.Container>
+            ...
+        </C.Container>
+    )
+}
