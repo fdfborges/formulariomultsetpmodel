b05466689cfd74ca79cb71f22ca79bec6df48658
Felipe de Freitas Borges <felipedefreitasborges@gmail.com>
2025-08-13 15:32:21 -0300

    Feito Step 1 funcion

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -1,15 +1,41 @@
+import { useNavigate } from 'react-router';
 import * as C from './styles';
+import { useForm, FormActions } from '../../contexts/FormContext';
 import { Theme } from '../../components/Theme/';
+import { ChangeEvent, useEffect } from 'react';
+
+
 
 export const FormStep1 = () => {
-    const handleNextStep = () =>{
-        
+    const navigate = useNavigate();
+    const { state, dispatch } = useForm();
+
+    useEffect(() => {
+        dispatch({
+            type: FormActions.setCurrentStep,
+            payload: 1
+        });
+    }, []);
+
+    const handleNextStep = () => {
+        if(state.name !== ""){
+            navigate("/step2");
+        } else{
+            alert("Preencha os dados!");
+        }
     }
-    
+
+    const handleNameChange = (e: ChangeEvent<HTMLInputElement>) => {
+        dispatch({
+            type: FormActions.setName,
+            payload: e.target.value
+        })
+    }
+
     return (
         <Theme>
             <C.Container>
-                <p>Passo 1/3</p>
+                <p>Passo {state.currentStep}/3</p>
                 <h1>Vamos começar com seu nome</h1>
                 <p>Preencha o campo abaixo com seu nome completo:</p>
 
@@ -20,6 +46,8 @@ export const FormStep1 = () => {
                     <input
                         type="text"
                         autoFocus
+                        value={state.name}
+                        onChange={handleNameChange}
                     />
                 </label>
 
