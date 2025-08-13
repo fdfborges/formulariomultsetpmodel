5d9ff6c27e6be717ef9b866bb001b4c9bcbd11d2
Felipe de Freitas Borges <felipedefreitasborges@gmail.com>
2025-08-13 15:16:21 -0300

    Style Step1 ok

--- src/pages/FormStep1/index.tsx
+++ src/pages/FormStep1/index.tsx
@@ -2,10 +2,28 @@ import * as C from './styles';
 import { Theme } from '../../components/Theme/';
 
 export const FormStep1 = () => {
+    const handleNextStep = () =>{
+        
+    }
+    
     return (
         <Theme>
             <C.Container>
-                Passo 1
+                <p>Passo 1/3</p>
+                <h1>Vamos começar com seu nome</h1>
+                <p>Preencha o campo abaixo com seu nome completo:</p>
+
+                <hr />
+
+                <label>
+                    Seu nome completo
+                    <input
+                        type="text"
+                        autoFocus
+                    />
+                </label>
+
+                <button onClick={handleNextStep}>Próximo</button>
             </C.Container>
         </Theme>
     )
